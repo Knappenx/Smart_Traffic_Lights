a12d3b70a6fb0bf5c94c1c10da14268c2382d42f
Rodolfo Frías <rodofriaslugo@gmail.com>
2021-11-26 23:44:23 -0600

    feat: cars detector

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.videoplayer import VideoPlayer
 from kivy.lang import Builder
 
 Builder.load_file('styles/main.kv')
@@ -10,5 +12,13 @@ class WindowLayout(Widget):
 
 class MainApp(App):
     def build(self):
+        # start playing the video at creation
+        #video = VideoPlayer(source='./car_detector/contituyentes_demo.mp4', state='play')
+
+        # create the video, and start later
+        #source = './car_detector/contituyentes_demo.mp4'
+        #video = VideoPlayer(source=source)
+        # and later
+        #video.state = 'play'
         return WindowLayout()
 MainApp().run()
