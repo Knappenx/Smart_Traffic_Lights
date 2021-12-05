0b33e5c3ceaf75fe6bc5cbf99fcd076144ac8ce3
Rodolfo Frías <rodofriaslugo@gmail.com>
2021-12-04 23:08:51 -0600

    feat: run multiple instances of car_detector in parallel using threads

--- car_detector/main.py
+++ car_detector/main.py
@@ -1,55 +1,91 @@
 # -*- coding: utf-8 -*-
 
+from time import sleep
+import time
 import cv2
-print(cv2.__version__)
-
-class CarDetector:
-    def __init__(self, video_path, cascade_path) -> None:
-        print("[INFO] loading video...", video_path)
+import logging
+from threading import Thread
+from car_detector.traffic_light import TrafficLight
+class CarDetector(Thread):
+    def __init__(self, video_path, cascade_path, lock) -> None:
+        logging.info("Initializing car detector...")
+        Thread.__init__(self)
+        self.light = TrafficLight()
         self.cascade_path = cascade_path
         self.video_path = video_path
         self.cap = cv2.VideoCapture(self.video_path)
         self.car_cascade = cv2.CascadeClassifier(self.cascade_path)
-        self.__create_trackbar()
+        self.img = None
+        self.lock = lock
+        self.running = True
+        self.paused = False
+        # self.__create_trackbar()
 
-    def __parameter_selection(self, x):
-        pass
 
     def __create_trackbar(self):
         cv2.namedWindow("img")
         cv2.createTrackbar('Scale Factor', 'img', 5, 200, self.__parameter_selection)
         cv2.createTrackbar('Min Neighbours', 'img', 10, 100, self.__parameter_selection)
 
-    def start(self) -> None:
-        while True:
-            ret, img = self.cap.read()
-            if (type(img) == type(None)):
+    def run(self):
+        logging.info("Starting video...")
+        self.__reproduce_video()
+
+    def __reproduce_video(self) -> None:
+        # self.lock.acquire()
+        start_time = time.clock()
+        while self.running:
+            # seconds = int(time.clock() - start_time)
+            # print('Seconds: ', seconds)
+            ret, self.img = self.cap.read()
+            if (type(self.img) == type(None) and ret == False):
                 break
-            
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            scalefactor_track = cv2.getTrackbarPos('Scale Factor', 'img')
-            min_neigh_track = cv2.getTrackbarPos('Min Neighbours', 'img')
-
-            if scalefactor_track is 0:
-                scalefactor_track = 1
-            else:
-                cars = self.car_cascade.detectMultiScale(
-                    gray, 
-                    scaleFactor = (scalefactor_track + 100) / 100, # 1.25 
-                    minNeighbors = min_neigh_track, #1
-                    minSize=(60, 60), 
-                    flags=cv2.CASCADE_SCALE_IMAGE
-                )
-
-            print(f'Número de carros contados: {len(cars)}')
-
+            cars = self.__detect_cars(self.img)
+            # print(f'Número de carros contados: {len(cars)}')
             for (x, y, w, h) in cars:
-                cv2.rectangle(img, (x, y),(x + w,y + h),(0, 0, 255), 2)      
-            
-            img.save('frame.jpg')
-            cv2.imshow(self.video_path, img)
-            
+                cv2.rectangle(self.img, (x, y),(x + w, y + h),(0, 0, 255), 2)
+                
+            cv2.imshow(self.video_path, self.img)
+
+            if self.paused is True:
+                cv2.imshow(self.video_path, self.img)
+                self.light.green()
+                time.sleep(self.seconds)
+                self.paused = False
+
             if cv2.waitKey(33) == 27:
                 break
 
         cv2.destroyAllWindows()
+    
+    def __detect_cars(self, img) -> list:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        scalefactor_track = cv2.getTrackbarPos('Scale Factor', 'img')
+        min_neigh_track = cv2.getTrackbarPos('Min Neighbours', 'img')
+
+        if scalefactor_track is 0:
+            scalefactor_track = 1
+        else:
+            cars = self.car_cascade.detectMultiScale(
+                gray, 
+                scaleFactor = (125 + 100) / 100, # 1.25 
+                minNeighbors = 1, #1
+                minSize=(60, 60), 
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+        return cars
+
+    def pause(self, seconds):
+        logging.info("Pausing video...")
+        self.seconds = seconds
+        self.paused = True
+
+    def resume(self):
+        pass
+
+    def stop(self):
+        logging.info("Stopping video...")
+        self.running = False
+        # self.cap.release()
+        # cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 
 from os import name
 from car_detector.main import CarDetector
-from colorama import Fore, Back, Style
 import threading
-
-class TrafficLight:
-    def __init__(self) -> None:
-        pass
-    
-    def green(self) -> None:
-        print(Fore.GREEN + "Traffic Light is on")
-
-    def red(self) -> None:
-        print(Fore.RED + "Traffic Light is off")
-    
-    def yellow(self) -> None:
-        print(Fore.YELLOW + "Traffic Light is flashing")
+lock = threading.Lock()
+import logging
+import cv2
+import time
+logging.basicConfig(level=logging.INFO,
+                    format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+                    )
 
 class MainApp:
     def __init__(self) -> None:
-        # self.car_detector_one = CarDetector('./test_video.mp4', './car_detector/cars.xml')
-        self.car_detector_two = CarDetector('./contituyentes_demo.mp4', './car_detector/cars2.xml')
-        self.traffic_light = TrafficLight()
-        # self.__start_threads([self.car_detector_one.start, self.car_detector_two.start])
+        self.car_detector_one = CarDetector('./test_video.mp4', './car_detector/cars.xml', lock)
+        self.car_detector_two = CarDetector('./contituyentes_demo.mp4', './car_detector/cars2.xml', lock)
+        self.threads = []
 
     def start(self) -> None:
-        self.car_detector_two.start()
+        self.__start_threads([self.car_detector_one, self.car_detector_two])
+        time.sleep(5)
+        self.car_detector_one.pause(10)
 
-
-    ## TODO - Hasta nuevo aviso :v
     def __start_threads(self, items: list) -> list:
-        for item in items:
-            threading.Thread(target=item).start()
+        logging.info('Starting threads...')
+        for i, item in enumerate(items):
+            item.start()
+            print(item)
         
 if __name__ == "__main__":
     main_app = MainApp()
